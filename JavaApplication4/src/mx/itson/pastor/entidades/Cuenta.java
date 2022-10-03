35e2c897587835f29da9ac2d954eadf470141eb7
PoledAmaya <pyatq2001@gmail.com>
2022-10-03 08:56:32 -0700

    VERSION 2.0

---
+++ JavaApplication4/src/mx/itson/pastor/entidades/Cuenta.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.pastor.entidades;
+
+import java.util.List;
+
+/**
+ *
+ * @author pyatq
+ */
+public class Cuenta {
+    
+    private Cliente cliente;
+    private String numero;
+    private List<Movimiento> movimiento;
+    
+}
---
+++ JavaApplication4/src/mx/itson/pastor/enumerador/TipoMovimiento.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package mx.itson.pastor.enumerador;
+
+/**
+ *
+ * @author pyatq
+ */
+public enum TipoMovimiento {
+    
+    CARGO, ABONO
+    
+}
